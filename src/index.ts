8800c3f6b3caa88d68b9a3836917464f0329b9cf
Tyler Savery <tyler@theyoungastronauts.com>
2025-09-19 12:40:46 -0400

    export of types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { VfxClient } from './client/vfx-client';
 
+// Export main client
 export { VfxClient };
+
+// Export enums
+export { Network, TxType } from './constants';
+
+// Export types
+export type {
+  Keypair,
+  VfxAddress,
+  Transaction,
+  PaginatedResponse
+} from './types';
