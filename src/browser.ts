951c0e1cc6c6b5b7e0c1674d5baf5c7c8ebfbe1f
Tyler Savery <tyler@theyoungastronauts.com>
2025-09-19 15:28:03 -0400

    export updates

---
+++ src/browser.ts
@@ -0,0 +1,3 @@
+// Browser-compatible exports (no window object, just re-exports for bundling)
+export { VfxClient, Network, TxType } from './index';
+export type { Keypair, VfxAddress, Transaction, PaginatedResponse } from './index';
