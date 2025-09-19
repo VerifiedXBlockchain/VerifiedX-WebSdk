2429bd5a61c9177073366366be2e0cb077ae2648
Tyler Savery <tyler@theyoungastronauts.com>
2025-09-19 13:39:42 -0400

    removed window object binding

--- example/node-example/utils.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from "express"
-
-
-export const apiError = (res: Response, message: string, code = 403) => {
-    return res.status(code).json({ success: false, message: message })
-} 
--- src/browser.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import { VfxClient } from './index';
-
-(window as any).vfx = { VfxClient };
